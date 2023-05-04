ac267f47774db16f50c9c59c3c5803faf5a6a5dc
regorxxx <regorxxx@protonmail.com>
2023-05-04 17:10:46 +0200

    Moved inline script to new file.

---
+++ js/startup.js
@@ -0,0 +1,15 @@
+﻿'use strict';﻿﻿﻿
+//04/05/23
+// Wait for document to load
+document.addEventListener("DOMContentLoaded", function(event) {
+	// Set theme colors
+	if (Object.keys(xxx).length) {xxx.theme = ''; xxx.dynamicColor = true;}
+	else {xxx = {}; xxx.theme = ''; xxx.dynamicColor = true;}
+	$.getJSON('xxx/config-theme.json', function (data) {
+		for (let setting of data) {xxx[setting.option] = setting.value;}
+		if (xxx.theme && xxx.theme.length) {document.documentElement.setAttribute("data-theme", xxx.theme);}
+	});
+	// Set template version
+	$('#help_btn').attr('title', 'Help (' + version + ')');
+	$('#help_dlg').dialog('option', 'title', 'Help (' + version + ')');
+});
