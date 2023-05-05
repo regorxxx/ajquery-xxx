04c3155178845aa0634b5788184babfd38e7bf60
regorxxx <regorxxx@protonmail.com>
2023-05-06 00:48:57 +0200

    - Template no longer shows AJAX errors while reloading the web page, as long as it is able to identify the page is unloading. See Mozilla compatibility browser [list](https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event).

--- js/startup.js
+++ js/startup.js
@@ -1,7 +1,8 @@
 ﻿'use strict';﻿﻿﻿
-//04/05/23
+//06/05/23
+
 // Wait for document to load
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(e) {
 	// Set theme colors
 	if (Object.keys(xxx).length) {xxx.theme = ''; xxx.dynamicColor = true;}
 	else {xxx = {}; xxx.theme = ''; xxx.dynamicColor = true;}
@@ -12,4 +13,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	// Set template version
 	$('#help_btn').attr('title', 'Help (' + version + ')');
 	$('#help_dlg').dialog('option', 'title', 'Help (' + version + ')');
+});
+
+// Stop server syncing while reloading
+window.addEventListener("beforeunload", function(e) {
+	bUnloading = true;
 });
